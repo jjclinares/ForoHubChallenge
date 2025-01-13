649d11afa73f6ba69b96d1713e2353f6931467f4
JamerJL <163032648+jjclinares@users.noreply.github.com>
2025-01-13 12:35:42 -0500

    Create Project
    API REST de un foro -Challenge Oracle ONE-ALURA

---
+++ Foro-hub-challenge/src/test/java/com/jamerchallenge/Foro_hub_challenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamerchallenge.Foro_hub_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
